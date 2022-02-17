3ad6f27aa85b78f46b26bbb0cbd9415c50ee3ee1
Abhishekud <98076768+Abhishekud@users.noreply.github.com>
2022-02-17 06:24:10 -0930

    first commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject7")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject7")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3771ad5b-ecfc-47df-9ca7-f3cd0b807d57")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace Selenium_Automation
+{
+    [TestClass]
+    public class TestTextBox
+    {
+        public object PageFactory { get; private set; }
+
+        [TestMethod]
+
+
+        public void Test()
+        {
+            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+            var chromeOptions = new ChromeOptions();
+            //   chromeOptions.AddArguments(new List<string>() { "headless" });
+
+            chromeOptions.AddUserProfilePreference("download.prompt_for_download", false);
+            chromeOptions.AddUserProfilePreference("disable-popup-blocking", "true");
+            chromeOptions.AddUserProfilePreference("block_third_party_cookies", true);
+
+            var hdriver = new ChromeDriver();
+
+            // This will open up the URL
+            hdriver.Url = "https://www.geeksforgeeks.org/";
+            hdriver.GetScreenshot();
+
+        }
+    }
+}
